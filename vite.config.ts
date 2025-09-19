e7341252fc689e5a9a8fa34a8e96eddb4a80b2fb
Aayushi Singh <75681857+aayushisingh11@users.noreply.github.com>
2025-09-20 02:24:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/Team-Mavrix",
+    base: "/Team-Mavrix/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
